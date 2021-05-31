bf70c4a589f454b0f63c140cb1673b858010f64b
Alonefcp <fcuadra@ucm.es>
2021-05-31 13:00:34 +0200

    Se cambia el pitch al entrar y sonido al acercarse al enemigo

---
+++ MusicaAdaptativa/Assets/DistanceToEnemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class DistanceToEnemy : MonoBehaviour
+{
+
+    public Transform enemyTr;
+    public float maxDistance = 10.0f;
+
+    EventInstance health;
+    float parameter;
+    void Start()
+    {
+        health = RuntimeManager.CreateInstance("event:/HeartBeat");
+        health.start();
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(enemyTr.position, transform.position);
+
+        if (distance > maxDistance)
+        {
+            parameter = 0.0f;
+        }
+        else
+        {
+            
+            parameter = Mathf.Abs(1.0f - (distance / maxDistance));
+        }
+
+        health.setParameterByName("Health", parameter);
+
+    }
+}
---
+++ MusicaAdaptativa/Assets/ZonaPeligrosa.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
+
+public class ZonaPeligrosa : MonoBehaviour
+{
+    Bus danger;
+    public DayNightCycle dayNight;
+
+    void Start()
+    {
+        danger = RuntimeManager.GetBus("bus:/AllEvents");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            dayNight.GetDayInstance().setPitch(0.95f);
+
+            dayNight.GetNightInstance().setPitch(0.95f);
+        }
+    
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            dayNight.GetDayInstance().setPitch(1.0f);
+
+            dayNight.GetNightInstance().setPitch(1.0f);
+        }
+    }
+}
